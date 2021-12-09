87bc0c5eb147fff2e5bc6c83c4f288e615c94d41
naufal22hadi <naufal22hadi@gmail.com>
2021-12-09 23:31:41 +0700

    adding dependency.

--- app/src/main/java/com/example/journeyid/presentation/MainActivity.kt
+++ app/src/main/java/com/example/journeyid/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.journeyid
+package com.example.journeyid.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.journeyid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
